3d9fd81c005386e7a1bea4390a203f1525aa2d49
Benjamin Allen <ben@bittorrent.com>
2014-12-29 14:53:59 -0800

    overwrite package.json from CRUMBS task

--- tasks/package-crumbs.js
+++ tasks/package-crumbs.js
@@ -43,9 +43,37 @@ module.exports = function (grunt) {
     // with specific commits
     grunt.registerTask('crumbs:overwrite', function () {
         var done = this.async();
+        var pkg = grunt.file.readJSON('package.json');
         var crumbs = process.env[ENV_CRUMBS];
-        console.log(crumbs);
-        console.log(typeof crumbs);
-        console.log(JSON.parse(crumbs));
+
+        // ENV_CRUMBS must exist in environment
+        if (!crumbs) {
+            grunt.fail.warn('"' + ENV_CRUMBS + '" is not an environment variable.');
+            return;
+        }
+        // parse the crumbs from the environment
+        crumbs = JSON.parse(crumbs);
+        // check if there are any modules to overwrite
+        var crumbKeys = _.keys(crumbs);
+
+        if (!crumbKeys.length) {
+            grunt.log.warn('no crumb modules to overwrite in package.json');
+            return;
+        }
+
+        // find modules to overwrite in dependency dictionaries in package.json
+        _.each(['dependencies', 'devDependencies', 'optionalDependencies'], function (depsKey) {
+            _.each(pkg[depsKey], function (val, key, obj) {
+                if (val.indexOf('://') > 0 && _.contains(crumbKeys, key)) {
+                    // overwrite the commit-ish on the dependencies git url
+                    var url = val.split('#').slice(0, 1).concat(crumbs[key]).join('#');
+                    grunt.log.warn('Overwriting dependency url for %s in %s to %s', key, depsKey, url);
+                    obj[key] = url;
+                }
+            });
+        });
+
+        // write changes back to package.json file
+        grunt.file.write('package.json', JSON.stringify(pkg, null, 2));
     });
 };
