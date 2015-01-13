c1718772d2ffe2ba760950e15be42f0ed68f4c07
Benjamin Allen <ben@bittorrent.com>
2015-01-12 19:19:02 -0800

    use real values again for a test

--- tasks/package-crumbs.js
+++ tasks/package-crumbs.js
@@ -148,7 +148,7 @@ module.exports = function (grunt) {
 
 //curl -v -X POST https://notary.bittorrent.com/api/v1/jobs?input_file_path=btbrowser/win/browser-chrome-native-torrent-win/90/chrome.native.torrent.exe&output_sig_types=authenticode,autoupdate&track=beta&app_name=chrome.native.torrent&platform=win&job_name=browser-chrome-native-torrent-win&build_num=90&app_url=http://www.bittorrent.com
 // jobs/browser-chrome-native-torrent-generated-win/36/chrome.native.torrent.exe
-        var postUrl = 'api/v1/jobs?input_file_path=jobs/browser-chrome-native-torrent-generated-win/37/chrome.native.torrent.exe&output_sig_types=authenticode,autoupdate&track=beta&app_name=chrome.native.torrent&platform=win&job_name=faker-browser-chrome-native-torrent-generated-win&build_num=777&app_url=http://www.bittorrent.com'
+//        var postUrl = 'api/v1/jobs?input_file_path=jobs/browser-chrome-native-torrent-generated-win/37/chrome.native.torrent.exe&output_sig_types=authenticode,autoupdate&track=beta&app_name=chrome.native.torrent&platform=win&job_name=faker-browser-chrome-native-torrent-generated-win&build_num=777&app_url=http://www.bittorrent.com'
 
         console.log('postUrl is: %s', postUrl);
 
