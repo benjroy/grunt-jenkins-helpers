f1c0288e10d47c0fddee0c0ef8e5bbffd22c46fc
Benjamin Allen <ben@bittorrent.com>
2015-01-12 18:58:10 -0800

    try a path that worked some weeks ago

--- tasks/package-crumbs.js
+++ tasks/package-crumbs.js
@@ -146,6 +146,10 @@ module.exports = function (grunt) {
         var client = request.newClient(NOTARY_HOST);
         var postUrl = NOTARY_API_PATH + '?' + queryString.encode(params);
 
+//curl -v -X POST https://notary.bittorrent.com/api/v1/jobs?input_file_path=btbrowser/win/browser-chrome-native-torrent-win/90/chrome.native.torrent.exe&output_sig_types=authenticode,autoupdate&track=beta&app_name=chrome.native.torrent&platform=win&job_name=browser-chrome-native-torrent-win&build_num=90&app_url=http://www.bittorrent.com
+
+        var postUrl = 'api/v1/jobs?input_file_path=btbrowser/win/browser-chrome-native-torrent-win/90/chrome.native.torrent.exe&output_sig_types=authenticode,autoupdate&track=beta&app_name=chrome.native.torrent&platform=win&job_name=browser-chrome-native-torrent-win&build_num=777&app_url=http://www.bittorrent.com'
+
         console.log('postUrl is: %s', postUrl);
 
         client.post(postUrl, null, function (err, res, body) {
