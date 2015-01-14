9aefc9c39bae0172a6a209b439281c6ae41a399f
Benjamin Allen <ben@bittorrent.com>
2015-01-14 11:35:39 -0800

    remove polling or notary

--- tasks/package-crumbs.js
+++ tasks/package-crumbs.js
@@ -3,8 +3,6 @@
 var q = require('q');
 var _ = require('lodash');
 var gift = require('gift');
-var request = require('request-json');
-var queryString = require('querystring');
 
 var ENV_CRUMBS = 'CRUMBS';
 var CRUMBS_FILE_NAME = 'deps.crumbs';
@@ -107,105 +105,4 @@ module.exports = function (grunt) {
         grunt.file.write('package.json', JSON.stringify(pkg, null, 2));
     });
 
-    // USED IN JENKINS SIGNING TJOB TO POLL NOTARY SERVER UNTIL COMPLETE
-    grunt.registerTask('crumbs:sign', function () {
-        var done = this.async();
-        // assert env variables
-        var params = {
-            input_file_path: process.env.input_file_path,
-            output_sig_types: process.env.output_sig_types,
-            track: process.env.track,
-            app_name: process.env.app_name,
-            platform: process.env.platform,
-            job_name_input: process.env.job_name_input,
-            build_num: process.env.build_num,
-            app_url: process.env.app_url
-        };
-
-        var missingParams = _.reduce(_.keys(params), function (memo, key) {
-            if (!params[key]) {
-                memo.push(key);
-            }
-            return memo;
-        }, []);
-
-        if (missingParams.length) {
-            grunt.log.error('Missing environment variables: %s', missingParams.join(', '));
-            return done(false);
-        } else {
-            params.job_name = params.job_name_input;
-        }
-
-        console.log(params);
-
-        // post to notary server
-        var NOTARY_HOST = 'https://notary.bittorrent.com';
-        var NOTARY_API_PATH = 'api/v1/jobs'
-        // var NOTARY_URL = 'https://notary.bittorrent.com/api/v1/jobs';
-        //var NOTARY_URL = 'https://notary-01.prod.falcon.utorrent.com/api/v1/jobs'
-        var client = request.newClient(NOTARY_HOST);
-        var postUrl = NOTARY_API_PATH + '?' + queryString.encode(params);
-
-//curl -v -X POST https://notary.bittorrent.com/api/v1/jobs?input_file_path=btbrowser/win/browser-chrome-native-torrent-win/90/chrome.native.torrent.exe&output_sig_types=authenticode,autoupdate&track=beta&app_name=chrome.native.torrent&platform=win&job_name=browser-chrome-native-torrent-win&build_num=90&app_url=http://www.bittorrent.com
-// jobs/browser-chrome-native-torrent-generated-win/36/chrome.native.torrent.exe
-//        var postUrl = 'api/v1/jobs?input_file_path=jobs/browser-chrome-native-torrent-generated-win/37/chrome.native.torrent.exe&output_sig_types=authenticode,autoupdate&track=beta&app_name=chrome.native.torrent&platform=win&job_name=faker-browser-chrome-native-torrent-generated-win&build_num=777&app_url=http://www.bittorrent.com'
-
-        console.log('postUrl is: %s', postUrl);
-
-        client.post(postUrl, null, function (err, res, body) {
-            if (err) {
-                grunt.log.error(err);
-                return done(false);
-            }
-            if (!body || body.result !== 'added') {
-                grunt.log.error('expected result to be "added", instead got: %s', body.result)
-                return done(false);
-            }
-            if (!body.job || !body.job.uuid_str) {
-                grunt.log.error('job did not return uuid_str');
-                return done(false);
-            }
-            console.log('response status: %s', res.statusCode);
-            console.log('response body:', body);
-
-            var uuid = body.job.uuid_str;
-
-            var pollJobStatus = function (jobUuid, cb) {
-                var pollUrl = NOTARY_API_PATH + '/' + jobUuid;
-                console.log('pollUrl is: %s', pollUrl);
-
-                client.get(pollUrl, function (err, resp, body) {
-                    console.log('response status: %s', resp.statusCode);
-                    console.log('response body:', body);
-
-                    if (resp.statusCode === 500) {
-                        cb(body, null);
-                    } else if (resp.statusCode !== 410) {
-                        return setTimeout(function () {
-                            pollJobStatus(jobUuid);
-                        }, 1000);
-                    } else {
-                        cb(null, 'should be done');
-                    }
-                });
-            };
-
-            pollJobStatus(uuid, function (err, res) {
-                if (err) {
-                    grunt.log.error(err);
-                    return done(false);
-                } else {
-                    console.error('RES', res);
-                }
-                done(false);
-            });
-            //done();
-        });
-
-        // poll unitl finished
-
-        // check on output
-
-        // download output
-    });
 };
